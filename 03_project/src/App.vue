cd719cde232811ccebecd38e048a6cd6d2b302ff
ryuker <lifeisagamenotaproduct@gmail.com>
2024-09-27 14:03:51 +0200

    changed title and subtitle

--- 03_project/src/App.vue
+++ 03_project/src/App.vue
@@ -7,7 +7,7 @@
 
 <template>
   <Navbar />
-  <Hero title="Test Title" subTitle="Test Subtitle"/>
+  <Hero title="Become a Vue Dev" subTitle="Find the Vue job that fits your skills and needs"/>
   <HomeCards />
   <JobListings limit="6" showButton="true"/>
 </template>
