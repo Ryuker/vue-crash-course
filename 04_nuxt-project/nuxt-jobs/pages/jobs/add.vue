5e288131d5117cf196b7fdd23aad940c22d6f065
ryuker <lifeisagamenotaproduct@gmail.com>
2024-10-09 00:02:33 +0200

    added add-job page and dynamic edit-job page

---
+++ 04_nuxt-project/nuxt-jobs/pages/jobs/add.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Add a job page</h1>
+</template>
---
+++ 04_nuxt-project/nuxt-jobs/pages/jobs/edit/[id].vue
@@ -0,0 +1,7 @@
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<template>
+  <h1>Edit job page of {{ id }}</h1>
+</template>
