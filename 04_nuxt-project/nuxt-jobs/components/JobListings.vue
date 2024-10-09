65e619667b796b84517dbcb3a459e2b88050ae8e
ryuker <lifeisagamenotaproduct@gmail.com>
2024-10-09 15:48:15 +0200

    copied over props and template code to JobListings.vue

--- 04_nuxt-project/nuxt-jobs/components/JobListings.vue
+++ 04_nuxt-project/nuxt-jobs/components/JobListings.vue
@@ -1,3 +1,39 @@
+<script setup>
+
+defineProps({
+  limit: {
+    type: Number,
+    default: null
+  },
+  showButton: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
 <template>
-  <h1>JobListings Component</h1>
+  <section class="bg-green-50 px-4 py-10">
+      <div class="container-xl lg:container m-auto">
+        <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <!-- Show loading spinner while loading is true -->
+        <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+          <PulseLoader />
+        </div>
+        <!-- Show job listing when done loading -->
+        <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <!-- <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/> -->
+        </div>
+      </div>
+    </section>
+    <section class="m-auto max-w-lg my-10 px-6">
+    <NuxtLink v-if="showButton"
+      to="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+    >
+      View All Jobs
+    </NuxtLink>
+  </section>
 </template>
