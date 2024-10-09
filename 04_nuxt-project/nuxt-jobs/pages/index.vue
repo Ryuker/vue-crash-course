425149ead69e277ec9ba1e31715d85cf6e868f51
ryuker <lifeisagamenotaproduct@gmail.com>
2024-10-09 16:02:50 +0200

    made sure attributes were reactive

--- 04_nuxt-project/nuxt-jobs/pages/index.vue
+++ 04_nuxt-project/nuxt-jobs/pages/index.vue
@@ -2,5 +2,5 @@
   <h1>Home Page</h1>
   <Hero title="Become a Vue Dev" subTitle="Find the Vue job that fits your skills and needs" />
   <HomeCards />
-  <JobListings limit="3" showButton="true" />
+  <JobListings :limit="3" :showButton="true" />
 </template>
