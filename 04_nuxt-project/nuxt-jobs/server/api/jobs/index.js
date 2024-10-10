1480a1cb3e312658ce837f51895d76cdb4200b2f
ryuker <lifeisagamenotaproduct@gmail.com>
2024-10-10 15:31:28 +0200

    installed json-server to serve the api data mimicking an external api

--- 04_nuxt-project/nuxt-jobs/server/api/jobs/index.js
+++ 04_nuxt-project/nuxt-jobs/server/api/jobs/index.js
@@ -1,13 +1,9 @@
-import jobData from '../../data/jobs.json';
-
 export default defineEventHandler(async(event) => {
 
   // api call with private key
-  // const api_endpoint = 'some url';
-  // const { data } = await $fetch(api_endpoint);
+  const api_endpoint = 'http://localhost:5000/jobs';
+  const data = await $fetch(api_endpoint);
 
-  
-  const data = jobData;
   // return api data
-  return data.jobs;
+  return data;
 });
